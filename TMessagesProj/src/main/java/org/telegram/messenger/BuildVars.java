b4c0cab375e90138cbd6af44f3597c49dbd63214
KhanhTM <khanhtm@vng.com.vn>
2016-12-26 15:47:59 +0700

    add custom build

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -12,23 +12,31 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class BuildVars {
-
-    public static boolean DEBUG_VERSION = false;
+    public static boolean DEBUG_VERSION = true;
     public static boolean DEBUG_PRIVATE_VERSION = false;
-    public static boolean LOGS_ENABLED = false;
+    public static boolean LOGS_ENABLED = true;
     public static boolean USE_CLOUD_STRINGS = true;
     public static boolean CHECK_UPDATES = true;
     public static boolean TON_WALLET_STANDALONE = false;
     public static int BUILD_VERSION = 2065;
     public static String BUILD_VERSION_STRING = "7.0.0";
-    public static int APP_ID = 4;
-    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
+    public static boolean LOG_FILE = false;
+    public static int APP_ID = 15997; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "fddd35d4d7c6971f66b5e37d581ab248"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "fddd35d4d7c6971f66b5e37d581ab248";
+    public static String HOCKEY_APP_HASH_DEBUG = "fddd35d4d7c6971f66b5e37d581ab248";
     public static String APPCENTER_HASH = "a5b5c4f5-51da-dedc-9918-d9766a22ca7c";
     public static String APPCENTER_HASH_DEBUG = "f9726602-67c9-48d2-b5d0-4761f1c1a8f3";
-    //
     public static String SMS_HASH = DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT";
     public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
 
+    public static String GCM_SENDER_ID = "760348033672";
+    public static String SEND_LOGS_EMAIL = "email@gmail.com";
+    public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
+    public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_VERSION = "20150326";
+    
     static {
         if (ApplicationLoader.applicationContext != null) {
             SharedPreferences sharedPreferences = ApplicationLoader.applicationContext.getSharedPreferences("systemConfig", Context.MODE_PRIVATE);
